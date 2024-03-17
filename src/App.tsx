3358d910ca479485ba0b0b2be4f4e8c8518a5dd4
darshil929 <darshilt929@gmail.com>
2024-03-17 13:09:26 +0530

    app.tsx

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {setupPlayer, addTrack} from "../musicPlayerServices"
+
+function App(): React.JSX.Element {
+  const [isPlayerReady, setIsPlayerReady] = useState(false)
+
+  async function setup() {
+    let isSetup = await setupPlayer()
+
+    if (isSetup) {
+      await addTrack()
+    }
+
+    setIsPlayerReady(isSetup)
+  }
+
+  useEffect(() => {
+    setup()
+  }, [])
+  
+
+  return (
+    <SafeAreaView>
+      <StatusBar
+
+      />
+      <Text>test</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1
+  }
+});
+
+export default App;
